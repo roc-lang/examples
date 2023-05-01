44ffb6e91debe015548941cf148bc75ca0ee9efd
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-05-01 19:48:07 +0200

    set up hello world expect

---
+++ ci_scripts/all_tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+./roc_nightly/roc build ./examples/HelloWorld/main.roc
+
+expect ci/expect_scripts/HelloWorld.exp
