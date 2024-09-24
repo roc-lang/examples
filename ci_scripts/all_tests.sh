348ab5a6b0bdffff646b774c2919eb5ec5d92721
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2024-09-24 20:29:00 +0200

    allow unbound through flag

--- ci_scripts/all_tests.sh
+++ ci_scripts/all_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-set -euxo pipefail
+set -exo pipefail
 
 if [ -z "${ROC}" ]; then
   echo "ERROR: The ROC environment variable is not set.
